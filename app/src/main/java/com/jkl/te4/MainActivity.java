2479ef8b0d18728ca046988b9c2aa3461330b6c2
Kei98 <keisychaves@hotmail.com>
2018-05-05 22:47:25 -0600

    [no sé si hay cambios]

--- app/src/main/java/com/jkl/te4/MainActivity.java
+++ app/src/main/java/com/jkl/te4/MainActivity.java
@@ -2,6 +2,8 @@ package com.jkl.te4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+
+            }
+            });
     }
 
 }
