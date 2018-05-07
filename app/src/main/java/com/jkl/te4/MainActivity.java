aa0c9d96de7c4a0f6bc1ca92a803af61efe149c3
Kei98 <keisychaves@hotmail.com>
2018-05-06 22:16:18 -0600

    [final, sin comentar]

--- app/src/main/java/com/jkl/te4/MainActivity.java
+++ app/src/main/java/com/jkl/te4/MainActivity.java
@@ -13,7 +13,7 @@ import android.widget.TextView;
 
     public class MainActivity extends AppCompatActivity {
 
-        private int largo = 40;
+        public int largo = (int) (Math.random() * 20 + 20);
         private int cont = 0;
         private TextView[] txtnum;
         private int [] array = new int[largo];
@@ -24,60 +24,70 @@ import android.widget.TextView;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
+            int x = 0;
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
 
-            txtnum = new TextView[largo];
-            txtnum[0] = (TextView)findViewById(R.id.txtnum1);
-            txtnum[1] = (TextView)findViewById(R.id.txtnum2);
-            txtnum[2] = (TextView)findViewById(R.id.txtnum3);
-            txtnum[3] = (TextView)findViewById(R.id.txtnum4);
-            txtnum[4] = (TextView)findViewById(R.id.txtnum5);
-            txtnum[5] = (TextView)findViewById(R.id.txtnum6);
-            txtnum[6] = (TextView)findViewById(R.id.txtnum7);
-            txtnum[7] = (TextView)findViewById(R.id.txtnum8);
-            txtnum[8] = (TextView)findViewById(R.id.txtnum9);
-            txtnum[9] = (TextView)findViewById(R.id.txtnum10);
-            txtnum[10] = (TextView)findViewById(R.id.txtnum11);
-            txtnum[11] = (TextView)findViewById(R.id.txtnum12);
-            txtnum[12] = (TextView)findViewById(R.id.txtnum13);
-            txtnum[13] = (TextView)findViewById(R.id.txtnum14);
-            txtnum[14] = (TextView)findViewById(R.id.txtnum15);
-            txtnum[15] = (TextView)findViewById(R.id.txtnum16);
-            txtnum[16] = (TextView)findViewById(R.id.txtnum17);
-            txtnum[17] = (TextView)findViewById(R.id.txtnum18);
-            txtnum[18] = (TextView)findViewById(R.id.txtnum19);
-            txtnum[19] = (TextView)findViewById(R.id.txtnum20);
-            txtnum[20] = (TextView)findViewById(R.id.txtnum21);
-            txtnum[21] = (TextView)findViewById(R.id.txtnum22);
-            txtnum[22] = (TextView)findViewById(R.id.txtnum23);
-            txtnum[23] = (TextView)findViewById(R.id.txtnum24);
-            txtnum[24] = (TextView)findViewById(R.id.txtnum25);
-            txtnum[25] = (TextView)findViewById(R.id.txtnum26);
-            txtnum[26] = (TextView)findViewById(R.id.txtnum27);
-            txtnum[27] = (TextView)findViewById(R.id.txtnum28);
-            txtnum[28] = (TextView)findViewById(R.id.txtnum29);
-            txtnum[29] = (TextView)findViewById(R.id.txtnum30);
-            txtnum[30] = (TextView)findViewById(R.id.txtnum31);
-            txtnum[31] = (TextView)findViewById(R.id.txtnum32);
-            txtnum[32] = (TextView)findViewById(R.id.txtnum33);
-            txtnum[33] = (TextView)findViewById(R.id.txtnum34);
-            txtnum[34] = (TextView)findViewById(R.id.txtnum35);
-            txtnum[35] = (TextView)findViewById(R.id.txtnum36);
-            txtnum[36] = (TextView)findViewById(R.id.txtnum37);
-            txtnum[37] = (TextView)findViewById(R.id.txtnum38);
-            txtnum[38] = (TextView)findViewById(R.id.txtnum39);
-            txtnum[39] = (TextView)findViewById(R.id.txtnum40);
+
+
+            txtnum = new TextView[41];
+            txtnum[x] = (TextView)findViewById(R.id.txtnum1);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum2);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum3);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum4);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum5);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum6);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum7);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum8);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum9);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum10);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum11);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum12);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum13);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum14);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum15);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum16);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum17);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum18);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum19);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum20);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum21);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum22);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum23);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum24);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum25);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum26);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum27);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum28);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum29);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum30);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum31);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum32);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum33);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum34);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum35);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum36);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum37);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum38);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum39);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum40);
+            txtnum[++x] = (TextView)findViewById(R.id.txtnum41);
 
 
             for(int i = 0; i < largo; i++) {
                 array[i] = (int)(Math.floor(Math.random()*101));
             }
 
-            for(int i = 0; i < array.length; i++){
+            for(int i = 0; i < largo; i++){
                 txtnum[i].setText(String.valueOf(array[i]));
+            }
+
+            for(int i = largo; i < 40; i++) {
+                if (txtnum[i].getText().equals("u")) {
+                    txtnum[i].setBackgroundResource(R.drawable.circle_white);
+                }
             }
 
             ((TextView)findViewById(R.id.bsort)).setOnClickListener(new View.OnClickListener() {
@@ -103,7 +113,7 @@ import android.widget.TextView;
             cont++;
             AlertDialog.Builder builder;
             builder = new AlertDialog.Builder(MainActivity.this,android.R.style.Theme_Material_Dialog_Alert);
-            builder.setTitle("Alerta")
+            builder.setTitle("Warning")
                     .setMessage("Do you wanna kill the current sorting process and initialize a new one?")
                     .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                         @Override
@@ -161,8 +171,9 @@ import android.widget.TextView;
                     }
                 }
             },700);
-            txtnum[a].setBackgroundResource(R.drawable.circle_orange);
-            txtnum[b].setBackgroundResource(R.drawable.circle_orange);
+            txtnum[a].setBackgroundResource(R.drawable.circle_brown);
+            txtnum[b].setBackgroundResource(R.drawable.circle_brown);
             compare++;
+            txtnum[40].setText("Comparison amount:"+ compare);
         }
     }
